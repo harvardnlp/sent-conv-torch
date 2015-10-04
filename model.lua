fcc899a7be1e5094c920fd4b94ddbaee1552b7a4
Jeffrey Ling <jeffrey.r.ling@gmail.com>
2015-10-04 16:46:10 -0400

    changed to temporal conv

--- model.lua
+++ model.lua
@@ -1,5 +1,7 @@
 require 'torch'
 require 'nn'
+require 'fbcunn'
+require 'cunn'
 
 -- Hyperparameters
 local num_channels = 1
@@ -8,22 +10,19 @@ local filt_size = 3
 local num_classes = 2
 local dropout_p = 0.5
 
-local vocab_size = 21420 -- TODO(jeffreyling): find vocab size of word2vec
+local vocab_size = 21421 -- TODO(jeffreyling): find vocab size of word2vec
 local vec_size = 300 -- word2vec vector size
 
 local model = nn.Sequential()
 model:add(nn.LookupTable(vocab_size, vec_size)) -- LookupTable for word2vec
-model:add(nn.View(1, -1, vec_size)) -- reshape into channels for convolution
-model:add(nn.SpatialConvolution(num_channels, num_feat_maps, vec_size, filt_size))
+model:add(nn.TemporalConvolution(vec_size, num_feat_maps, filt_size))
 model:add(nn.ReLU())
-model:add(nn.Max(2)) -- max over second direction
+model:add(nn.Transpose({2,3})) -- swap feature maps and time
+model:add(nn.Max(3)) -- max over time
 
-model:add(nn.View(num_feat_maps))
 model:add(nn.Dropout(dropout_p))
 model:add(nn.Linear(num_feat_maps, num_classes))
 model:add(nn.ReLU())
 model:add(nn.LogSoftMax())
-
--- constrain L2 norm with ||w||_2 = s in gradient descent
 
 return model
