ef33fa16b7f7ac6352281320f0e832d71cc9b6d9
Jeffrey Ling <jeffrey.r.ling@gmail.com>
2015-10-06 01:49:50 -0400

    change to TemporalConvolutionFB; much faster training

--- model.lua
+++ model.lua
@@ -18,7 +18,8 @@ end
 function ModelBuilder:make_net(opts)
   local model = nn.Sequential()
   model:add(nn.LookupTable(opts.vocab_size, opts.vec_size)) -- LookupTable for word2vec
-  model:add(nn.TemporalConvolution(opts.vec_size, opts.num_feat_maps, opts.kernel_size))
+  --model:add(nn.TemporalConvolution(opts.vec_size, opts.num_feat_maps, opts.kernel_size))
+  model:add(nn.TemporalConvolutionFB(opts.vec_size, opts.num_feat_maps, opts.kernel_size))
   model:add(nn.ReLU())
   model:add(nn.Transpose({2,3})) -- swap feature maps and time
   model:add(nn.Max(3)) -- max over time
