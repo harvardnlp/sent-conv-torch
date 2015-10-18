58e05443dae74fcd3567c29939227fc77d2baad7
Jeffrey Ling <jeffrey.r.ling@gmail.com>
2015-10-18 02:37:37 -0400

    preprocess strings

--- make_hdf5.py
+++ make_hdf5.py
@@ -1,5 +1,6 @@
 import numpy as np
 import h5py
+import re
 
 def load_bin_vec(fname, vocab):
     """
@@ -25,6 +26,11 @@ def load_bin_vec(fname, vocab):
                 f.read(binary_len)
     return word_vecs
 
+def line_to_words(line):
+  clean_line = clean_str(line.strip())
+  words = clean_line.split(' ')
+  return words
+
 def load_data(pos_fname, neg_fname):
   max_sent_len = 0
   word_to_idx = {}
@@ -32,7 +38,7 @@ def load_data(pos_fname, neg_fname):
 
   pos_file = open(pos_fname, "r")
   for line in pos_file:
-      words = line.strip().split(' ')
+      words = line_to_words(line)
       max_sent_len = max(max_sent_len, len(words))
       for word in words:
           if not word in word_to_idx:
@@ -41,7 +47,7 @@ def load_data(pos_fname, neg_fname):
 
   neg_file = open(neg_fname, "r")
   for line in neg_file:
-      words = line.strip().split(' ')
+      words = line_to_words(line)
       max_sent_len = max(max_sent_len, len(words))
       for word in words:
           if not word in word_to_idx:
@@ -54,7 +60,7 @@ def load_data(pos_fname, neg_fname):
   neg_data = []
 
   for line in pos_file:
-      words = line.strip().split(' ')
+      words = line_to_words(line)
       sent = [word_to_idx[word] for word in words]
       if len(sent) < max_sent_len:
           sent.extend([1] * (max_sent_len - len(sent)))
@@ -62,7 +68,7 @@ def load_data(pos_fname, neg_fname):
       pos_data.append(sent)
 
   for line in neg_file:
-      words = line.strip().split(' ')
+      words = line_to_words(line)
       sent = [word_to_idx[word] for word in words]
       if len(sent) < max_sent_len:
           sent.extend([1] * (max_sent_len - len(sent)))
@@ -76,6 +82,26 @@ def load_data(pos_fname, neg_fname):
 
   return np.array(pos_data + neg_data, dtype=np.int32), np.array(labels, np.int32), word_to_idx
 
+def clean_str(string, TREC=False):
+  """
+  Tokenization/string cleaning for all datasets except for SST.
+  Every dataset is lower cased except for TREC
+  """
+  string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)     
+  string = re.sub(r"\'s", " \'s", string) 
+  string = re.sub(r"\'ve", " \'ve", string) 
+  string = re.sub(r"n\'t", " n\'t", string) 
+  string = re.sub(r"\'re", " \'re", string) 
+  string = re.sub(r"\'d", " \'d", string) 
+  string = re.sub(r"\'ll", " \'ll", string) 
+  string = re.sub(r",", " , ", string) 
+  string = re.sub(r"!", " ! ", string) 
+  string = re.sub(r"\(", " \( ", string) 
+  string = re.sub(r"\)", " \) ", string) 
+  string = re.sub(r"\?", " \? ", string) 
+  string = re.sub(r"\s{2,}", " ", string)    
+  return string.strip() if TREC else string.strip().lower()
+
 if __name__ == '__main__':
   data, labels, word_to_idx = load_data("rt-polarity.pos", "rt-polarity.neg")
   w2v = load_bin_vec("/n/rush_lab/data/GoogleNews-vectors-negative300.bin", word_to_idx)
@@ -87,10 +113,10 @@ if __name__ == '__main__':
   # Word embeddings initialized to random Unif(-0.25, 0.25)
   embed = (embed - 0.5) / 2
   for word, vec in w2v.items():
-    embed[word_to_idx[word]] = vec
+    embed[word_to_idx[word] - 1] = vec
 
   N = data.shape[0]
-  print 'data size:', N
+  print 'data size:', data.shape
   perm = np.random.permutation(N)
   data = data[perm]
   labels = labels[perm]
